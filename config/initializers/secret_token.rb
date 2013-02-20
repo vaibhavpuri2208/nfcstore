a6a27df3f54e8e31375bf437a86b82c7a891c6e9
vaibhav puri <vaibhav@taggito.com>
2013-02-20 11:57:23 +0100

    first commit (project start)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nfcstore::Application.config.secret_token = '9788f66f6c1369e3b70d34f1ff8b66c1f12bd86f6a0cb9732db2984efce1e065ef895537ec4eecec0dbf6cb648fee15aa14784e7547afd1a51e969edd7af7bad'
